c0c9df66c2ade90c28ac98b6a6b5e6e1c39b8f44
shenny@x470 <shenny88@gmail.com>
2020-12-31 02:46:34 +0530

    string sorting in comma

---
+++ cs4/10_sort_string.py
@@ -0,0 +1,8 @@
+# 10. Write a program that accepts a comma separated sequence of words as input and
+#    prints the words in a comma-separated sequence after sorting them alphabetically.
+mystr = input("Enter string seperated by comma: ")
+mystr_list = sorted(mystr.split(","))
+mystr = ",".join(mystr_list)
+print(mystr)
+
+
